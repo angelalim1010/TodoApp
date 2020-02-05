67176c53df272a9fbe6f53c3de5719a8d7efe37e
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-02-05 16:53:46 -0500

    adding handlebars conditional from original json

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ let rawdata = fs.readFileSync('data.json');
 let dataObj= JSON.parse(rawdata);
 console.log(dataObj);
 
+hbs.registerHelper('if_eq', function(a, b, opts) {
+    if (a == b) {
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
+
 app.get('/', (req,res)=>{
     res.render('indexTemplate.hbs',{object : dataObj} );
 });
