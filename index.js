a0cd056fe56310a790b91cb42bc056037b1044fa
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-02-04 22:32:37 -0500

    adding handlebars list to html

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require('express');
-const app = express();
-const fs = require('fs');
-const handlebars = require('handlebars');
 
-// fs.readFile('data.json', (err, data)=>{
-//     if (err) throw err;
-//     let activity = JSON.parse(data);
-//     console.log(activity);
-// })
+const fs = require('fs');
+const path = require('path');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('view engine','hbs');
+app.set('views', path.join(__dirname,'/views'));
+
+
 let rawdata = fs.readFileSync('data.json');
 let dataObj= JSON.parse(rawdata);
 console.log(dataObj);
-let source = "<ul> {{#each object}} <li>{{name}}</li> {{/each}} </ul>";
-let wrapper = {object : dataObj};
-let template = handlebars.compile(source);
- 
-// var data = { "name": "Alan", "hometown": "Somewhere, TX",
-//              "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
-let result = template(wrapper);
-console.log(result);
-
-app.use(express.static('public'));
 
 app.get('/', (req,res)=>{
-    res.send("hello");
+    res.render('indexTemplate.hbs',{object : dataObj} );
 });
 
-app.listen(3001,()=> console.log('App listening on port 3000'));
+app.listen(3001,()=> console.log('App listening on port 3001'));
