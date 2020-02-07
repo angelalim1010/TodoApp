7bbddd7507374ea3f18f10a08535ac5a550d8f6c
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-02-07 14:42:01 -0500

    added get request with param username

--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,20 @@ const axios = require("axios")
 
 // const API_URL = process.env.API_URL;
 
-router.get("/user", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
 	try {
 		const user = await axios.get("https://hunter-todo-api.herokuapp.com/user");
 		res.status(200).json(user.data);
 	} catch(err) {
+		console.log(err);
+	}
+});
+
+router.get("/:username", async(req,res,next)=>{
+	try{
+		const username = await axios.get(`https://hunter-todo-api.herokuapp.com/user?username=${req.params.username}`) 
+		res.status(200).json(username.data);
+	}catch(err){
 		console.log(err);
 	}
 });
