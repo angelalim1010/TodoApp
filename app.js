2eaa56d9f2457ffa3798c049f3cf1633481f8376
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-02-25 16:07:10 -0500

    adding  process.env.PORT

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.use(function(req,res){
 });
 
 
-app.listen(3000,()=> console.log('App listening on port 3000'));
+app.listen(process.env.PORT || 3000,()=> console.log('App listening on port 3000'));
 
 module.exports = app;
